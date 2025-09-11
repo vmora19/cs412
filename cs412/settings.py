6408403593a4b6263ab6c7e942b920ac06e8bc27
Valentina Mora <vmora19@bu.edu>
2025-09-11 16:04:28 -0400

    updated static url to include username

--- cs412/settings.py
+++ cs412/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #declarations to reference static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/vmora19/static/'
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, "static")
 ]
